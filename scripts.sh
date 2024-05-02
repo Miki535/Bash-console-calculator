d87ae87f9e821c46826bab57716712deb675ef75
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-05-02 22:33:08 +0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-echo "/mEnter first number:"
+
+while [ "$choose" != "exit" ]
+do
+
+echo "Enter first number:"
 read number1
 
-echo "/mEnter second number:"
+echo "Enter second number:"
 read number2
 
-echo $number1
-echo $number2
 
-#echo "+, -, /, *"
-#read $choose
+echo "+, -, /, *"
+read choose
 
-#if 
+if [ $choose = + ]; then
+result=$(($number1+$number2))
+echo $result
+else 
+echo "TST"
+fi
+#exit cycle
+done
