ceeb5d0fa046682544dbfb05f498659104b1c2b0
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-05-02 22:55:57 +0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 
-while [ "$choose" != "exit" ]
-do
+while [ "$choose" != "exit" ]; do
 
-echo "Enter first number:"
-read number1
+    echo "Enter first number:"
+    read number1
 
-echo "Enter second number:"
-read number2
+    echo "Enter second number:"
+    read number2
 
 
-echo "+, -, /, *"
-read choose
+    echo "+, -, /, *"
+    read choose
 
-if [ $choose = + ]; then
-result=$(($number1+$number2))
-echo $result
-else 
-echo "TST"
-fi
-#exit cycle
+    if [ $choose = + ]; then
+        result=$(($number1+$number2))
+        echo $result
+    elif [ $choose = exit ]; then
+        echo "Bye!"
+    else 
+        echo "Undefined operation!"
+    fi
 done
