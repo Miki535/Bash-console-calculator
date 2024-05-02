8437ff2f05eb49e237eb5af90d8e176ddf79e1b0
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-05-02 22:13:25 +0300

    Create scripts.sh  and testing it

---
+++ scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "/mEnter first number:"
+read number1
+
+echo "/mEnter second number:"
+read number2
+
+echo $number1
+echo $number2
+
+#echo "+, -, /, *"
+#read $choose
+
+#if 
